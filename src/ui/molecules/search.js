b17eb56254452b1c81db709194f2a5b9ae479da6
Max <a4ron5@yandex.ru>
2018-04-15 13:15:21 +0500

    ui: add searchbox

---
+++ src/ui/molecules/search.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: 'Roboto';
+  padding: 10px 20px;
+  margin-left: auto;
+`
+const WrapperOne = styled.div`
+  width: 400px;
+  background-color: gray;
+  opacity: 0.4;
+  border-radius: 7px;  
+`
+
+const SearchIcon = styled.i``;
+
+const Input = styled.input`
+  width: 91%;
+	background: transparent;
+	border: 0;
+  font: inherit;
+  font-size: 18px;
+  color: white;
+  outline: 0;
+  padding: 10px 20px;
+`
+
+const Button = styled.button` 
+  background: transparent;
+	border: 0;
+	color: inherit;
+	font: inherit;
+	outline: 0;
+`
+export const Search = ({onChangeInput, onClickButton}) => (
+  <Wrapper>
+    <WrapperOne>
+      <Input onChange={onChangeInput}/>
+      <Button onClick={onClickButton}>
+        <SearchIcon className='fa fa-search'></SearchIcon>
+      </Button>
+    </WrapperOne>
+  </Wrapper>
+)
