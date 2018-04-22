79f5275ba2d02ef73b5629c538578b29202bd2e4
Max <a4ron5@yandex.ru>
2018-04-22 17:13:14 +0500

    feat: add search

--- src/features/search/search.js
+++ src/features/search/search.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import styled from 'styled-components'
-
+import { SearchUI } from '../../ui/molecules'
 
 export class Search extends React.Component {
   
+  state = {
+    text: ''
+  }
+
+  buttonClick = () => {
+    console.log(this.state.text)
+  }
+
+  inputChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
   
   render() {
     return (
-      <h1>
-        F
-      </h1>
+      <SearchUI 
+        onClickButton={this.buttonClick}
+        onChangeInput={this.inputChange}        
+      ></SearchUI>
     )
   }
 }
