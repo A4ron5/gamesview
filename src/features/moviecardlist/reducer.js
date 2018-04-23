a0c25cd7c0e3c365ec7d2d363e8cd9925d5e062d
Max <a4ron5@yandex.ru>
2018-04-23 17:43:51 +0500

    feat(moviecardlist): add reducer

---
+++ src/features/moviecardlist/reducer.js
@@ -0,0 +1,29 @@
+import { 
+  FETCH_MOVIES_DISCOVER_REQUEST, 
+  FETCH_MOVIES_DISCOVER_SUCCESS, 
+  FETCH_MOVIES_DISCOVER_FAILURE
+ } from './action';
+
+export const discover = (state = { isFetching: false, items: [] }, action) => {
+  switch(action.type) {
+    case FETCH_MOVIES_DISCOVER_REQUEST:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case FETCH_MOVIES_DISCOVER_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        items: action.items
+      }
+    case FETCH_MOVIES_DISCOVER_FAILURE:
+      return {
+        ...state,
+        error: action.error
+      }
+    default:
+      return state
+  }
+}
+
