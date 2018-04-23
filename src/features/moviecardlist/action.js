a3e0d287002e7f6f02a049dde0c13f3c68e2c67b
Max <a4ron5@yandex.ru>
2018-04-23 17:43:43 +0500

    feat(moviecardlist): add action

---
+++ src/features/moviecardlist/action.js
@@ -0,0 +1,26 @@
+import api from '../../api'
+
+export const FETCH_MOVIES_DISCOVER_REQUEST = 'FETCH_MOVIES_DISCOVER_REQUEST';
+export const FETCH_MOVIES_DISCOVER_SUCCESS = 'FETCH_MOVIES_DISCOVER_SUCCESS';
+export const FETCH_MOVIES_DISCOVER_FAILURE = 'FETCH_MOVIES_DISCOVER_FAILURE';
+
+export const fetchMoviesDiscoverRequest = () => ({
+  type: FETCH_MOVIES_DISCOVER_REQUEST
+});
+
+export const fetchMoviesDiscoverSuccess = (data) => ({
+  type: FETCH_MOVIES_DISCOVER_SUCCESS,
+  items: data
+});
+
+export const fetchMoviesDiscoverFailure = (error) => ({
+  type: FETCH_MOVIES_DISCOVER_FAILURE,
+  error
+})
+
+export const discoverMovies = query => dispatch => {
+  dispatch(fetchMoviesDiscoverRequest());
+
+  return api.discoverMovies()
+      .then(data => dispatch(fetchMoviesDiscoverSuccess(data)));
+}
