9695b668e44a318b37c5653967e824dbed19c68e
Max <a4ron5@yandex.ru>
2018-04-28 19:30:12 +0500

    api: fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,35 @@
 import axios from 'axios'
 
 const URL = 'https://api.themoviedb.org/3';
-const KEY = '8c9f0b25d628dc3a96bcc112d2c82e63';
+const API_KEY = '8c9f0b25d628dc3a96bcc112d2c82e63';
+
+const discoverMovies = () => {
+  const params = {
+    api_key: API_KEY,
+  }
+
+  return axios.get(`${URL}/discover/movie`, { params });
+}
 
 const searchMovies = (query) => {
   const params = {
     query,
-    api_key: KEY,
+    api_key: API_KEY,
   };
 
   return axios.get(`${URL}/search/movie`, { params });
 }
 
-const discoverMovies = () => {
+const fetchMovie = (id) => {
   const params = {
-    api_key: KEY,
+    api_key: API_KEY,
   }
 
-  return axios.get(`${URL}/discover/movie`, { params })
+  return axios.get(`${URL}/movie/${id}`, { params });
 }
 
 export default {
   searchMovies,
-  discoverMovies
+  discoverMovies,
+  fetchMovie
 }
